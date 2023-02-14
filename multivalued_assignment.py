b263bd441feaf106770f17032bf4f3c0dae3da26
Pavlov83 <resolversoft@gmail.com>
2023-02-14 17:33:01 +0200

    multivalued assignment

---
+++ multivalued_assignment.py
@@ -0,0 +1,18 @@
+x,y,z = 3,4,5
+
+print(y)
+
+(x,y,z) = [7,8,9]
+
+print(y)
+
+# we can use multivalue assignment to swap values
+
+left,middle,right = 'first','second','third'
+
+print(left,middle,right)
+
+left,middle,right = middle,right,left
+
+print(left,middle,right)
+
